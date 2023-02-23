65aefb1cf94fa7555fb81dd901b664a70260982c
Ce Gao <cegao@tensorchord.ai>
2023-02-23 16:55:56 +0800

    feat: First commit
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Modelz</span>,
+  project: {
+    link: 'https://modelz.ai',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/tensorchord/modelz-docs',
+  footer: {
+    text: '@TensorChord Inc.',
+  },
+}
+
+export default config
