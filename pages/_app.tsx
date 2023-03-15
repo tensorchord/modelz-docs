2511d3af210be2b11e19139774a7aca54a5cea9a
Jinjing.Zhou <allenzhou@tensorchord.ai>
2023-03-15 22:09:04 +0800

    add GA
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+// pages/_app.js
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
